fd7c5d998b182d8aa7fc0057b69e43f4044e612a
joanthan <joanthan114311@gmail.com>
2026-08-14 07:19:26 -0500

    publict routes done

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router"
+function LoginPage (){
+    return(
+    <>
+        <h1> Login Page</h1>
+        <Link to="/register">register</Link>
+    </>
+)
+}
+
+export default LoginPage
---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router"
+function RegisterPage (){
+    return(
+    <>
+        <h1> Register Page</h1>
+         <Link to="/">Login</Link>
+    </>
+)
+}
+
+export default RegisterPage
